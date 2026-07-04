07dc93f91f97a751097c1b3e0255f3a03f6f0d62
FamisAND <FamisAND@users.noreply.github.com>
2026-07-04 15:24:50 +0200

    Ajusta horas de sala y ticket medio

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@
  *     y recargar.
  */
 
-const CACHE_VERSION = 'mis-dashboards-v207';
+const CACHE_VERSION = 'mis-dashboards-v208';
 
 // Recursos que se pre-cachean en install.
 // Rutas RELATIVAS al scope del SW (que es el directorio donde vive este archivo).
