e60658f39ec472bbb56ae8f34bdb765c0531fe65
FamisAND <FamisAND@users.noreply.github.com>
2026-07-05 23:52:00 +0200

    Mejora sizing y tarjetas ifAsign

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@
  *     y recargar.
  */
 
-const CACHE_VERSION = 'mis-dashboards-v210';
+const CACHE_VERSION = 'mis-dashboards-v211';
 
 // Recursos que se pre-cachean en install.
 // Rutas RELATIVAS al scope del SW (que es el directorio donde vive este archivo).
