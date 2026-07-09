429807d8285cb629fb4fc8ff075223e731fcea12
FamisAND <FamisAND@users.noreply.github.com>
2026-07-09 23:14:22 +0200

    Corrige contraste del PDF de facturas

--- service-worker.js
+++ service-worker.js
@@ -21,7 +21,7 @@
  *     y recargar.
  */
 
-const CACHE_VERSION = 'mis-dashboards-v217';
+const CACHE_VERSION = 'mis-dashboards-v218';
 
 // Recursos que se pre-cachean en install.
 // Rutas RELATIVAS al scope del SW (que es el directorio donde vive este archivo).
